312d84245588364b21d1d831c58c1aaa47269072
Srinivasan Ashwyn Srinivasan <ashwynsrinivasan@icloud.com>
2025-06-17 16:35:42 -0400

    Fixing CLM module

--- clm/openlight_laser_module/krishna_src_code/euler.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# Function to time step on a state space system
-def euler(A, B, C, D, dt, xkm1, uk):
-    Na = np.shape(A)
-    xk = np.linalg.solve((np.eye(Na[0]) - dt*A), (xkm1 + dt*np.dot(B,uk)))
-    yk = C*xk + D*uk
-    return yk, xk
--- clm/openlight_laser_module/krishna_src_code/main.py
+++
@@ -1,8 +0,0 @@
-
-
-if __name__ == '__main__':
-    # Run try_sys_sim.py
-    from try_sys_sim import main
-    main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
